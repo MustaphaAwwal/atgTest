c96c2a82ebe651cd05bffc8b3ac144cadba318bd
Mustapha Awwal <awwalmustapha41@gmail.com>
2023-04-17 06:25:10 +0100

    Update test.py

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ class test_ATGWorld(unittest.TestCase):
 		self.chrome_options = Options()
 		
 		self.chrome_options.add_argument('--no-sandbox')
-# 		self.chrome_options.add_argument('--headless')
+ 		self.chrome_options.add_argument('--headless')
 # 		self.chrome_options.add_argument('--allow-insecure-localhost')
 # 		self.chrome_options.add_argument('--disable-gpu')
 # 		self.chrome_options.add_argument('--ignore-certificate-errors')
