82e11e29dad94fbb322187f8c417f0980f6e1c69
Mustapha Awwal <awwalmustapha41@gmail.com>
2023-04-17 06:15:21 +0100

    Update test.py

--- test.py
+++ test.py
@@ -16,11 +16,11 @@ class test_ATGWorld(unittest.TestCase):
 		
 		self.chrome_options.add_argument('--no-sandbox')
 		self.chrome_options.add_argument('--headless')
-		self.chrome_options.add_argument('--allow-insecure-localhost')
-		self.chrome_options.add_argument('--disable-gpu')
-		self.chrome_options.add_argument('--ignore-certificate-errors')
-		self.chrome_options.add_argument('--allow-running-insecure-content')
-		self.chrome_options.add_argument('--disable-setuid-sandbox')
+# 		self.chrome_options.add_argument('--allow-insecure-localhost')
+# 		self.chrome_options.add_argument('--disable-gpu')
+# 		self.chrome_options.add_argument('--ignore-certificate-errors')
+# 		self.chrome_options.add_argument('--allow-running-insecure-content')
+# 		self.chrome_options.add_argument('--disable-setuid-sandbox')
 		self.chrome_options.add_argument('--disable-dev-shm-usage')
 		self.driver = webdriver.Chrome(service=Service('/usr/bin/chromedriver'), options=self.chrome_options)
 
