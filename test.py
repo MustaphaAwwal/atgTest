cecfb7aec7774327e39e3130bf637ee5feb6bf9b
Mustapha Awwal <awwalmustapha41@gmail.com>
2023-04-17 07:20:21 +0100

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ class test_ATGWorld(unittest.TestCase):
 		self.chrome_options.add_argument('--headless'); 
 		self.chrome_options.add_argument('--disable-dev-shm-usage') 
 		self.chrome_options.add_argument('--window-size=1920x1080')
-		
+		self.chrome_options.add_argument('--disable-gpu')
 		
 		self.driver = webdriver.Chrome(service=Service('/usr/bin/chromedriver'), options=self.chrome_options)
 
