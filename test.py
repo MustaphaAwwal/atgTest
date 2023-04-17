95edaf10c5307ea276a1608c114df9dc837311e1
Mustapha Awwal <awwalmustapha41@gmail.com>
2023-04-17 11:09:23 +0100

    Update test.py

--- test.py
+++ test.py
@@ -11,25 +11,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class test_ATGWorld(unittest.TestCase):
-	def setUp(self):
-		self.chrome_options = Options()
-		
-		
-		 
-		
-		
-		self.driver = webdriver.Chrome(service=Service('/usr/bin/chromedriver'), options=self.chrome_options)
+        def setUp(self):
+                self.chrome_options = Options()
+                self.chrome_options.add_argument('--headless')
+                self.chrome_options.add_argument('--no-sandbox')
+                self.chrome_options.add_argument('--disable-dev-shm-usage')
+                self.chrome_options.add_argument('--disable-gpu')
+                self.driver = webdriver.Chrome(service=Service('/usr/bin/chromedriver'), options=self.chrome_options)
 
-	def test_atg_website(self):
-		self.driver.get("https://atg.world/")
-		print('Testing the page title')
-		self.assertEqual(self.driver.title, "Across The Globe (ATG) - Professional and Personal Social Networking", 'wrong page title')
-		print("Testing for page content using h2 element CSS_SELECTOR")
-		self.assertTrue(  WebDriverWait(self.driver, 10).until(
-			EC.presence_of_element_located((By.CSS_SELECTOR, "h2"))
-			), 'h2 element not present')
-	def tearDown(self):
-		self.driver.quit()
+        def test_atg_website(self):
+                self.driver.get("https://atg.world/")
+                print('Testing the page title')
+                self.assertEqual(self.driver.title, "Across The Globe (ATG) - Professional and Personal Social Networking", 'wrong page title')
+                print("Testing for page content using h2 element CSS_SELECTOR")
+                self.assertTrue(  WebDriverWait(self.driver, 10).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, "h2"))
+                        ), 'h2 element not present')
+        def tearDown(self):
+                self.driver.quit()
 
 if __name__ == "__main__":
-	unittest.main()
+        unittest.main()
